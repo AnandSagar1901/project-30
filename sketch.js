413d87a65e20bf63d1f253c9c067d5c05a5f370c
AnandSagar1901 <78773233+AnandSagar1901@users.noreply.github.com>
2021-07-26 16:20:56 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function preload(){
   polygon_img=loadImage("polygon.png");
 }
 function setup() {
-  createCanvas(900,400);
+  createCanvas(displayWidth,displayHeight);
   engine = Engine.create();
   world = engine.world;
   Engine.run(engine);
